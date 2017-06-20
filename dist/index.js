25ba4ca8dc0eba3d96eab9e3d583dd7f2e356acf
bersling <bersling@gmail.com>
2017-06-20 16:59:44 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var hello_world_1 = require("./hello-world");
-exports.HelloWorld = hello_world_1.HelloWorld;
+const $ = require("jquery");
+var MathQuill;
+(function (MathQuill) {
+    function loadMathQuill() {
+        loadCss('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css');
+        loadJs('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css');
+    }
+})(MathQuill = exports.MathQuill || (exports.MathQuill = {}));
+function loadCss(url) {
+    var cssId = 'mathquill'; // you could encode the css path itself to generate id..
+    if (!document.getElementById(cssId)) {
+        var head = document.getElementsByTagName('head')[0];
+        var link = document.createElement('link');
+        link.id = cssId;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        link.media = 'all';
+        head.appendChild(link);
+    }
+}
+function loadJs(url) {
+    $.getScript(url, function (data, textStatus, jqxhr) {
+        console.log(data); // Data returned
+        console.log(textStatus); // Success
+        console.log(jqxhr.status); // 200
+        console.log("Load was performed.");
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-export {HelloWorld} from './hello-world'
+import * as $ from 'jquery'
 
+export namespace MathQuill {
+  function loadMathQuill() {
+    loadCss('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css');
+    loadJs('https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css');
+  }
+}
+
+function loadCss(url: string) {
+  var cssId = 'mathquill';  // you could encode the css path itself to generate id..
+  if (!document.getElementById(cssId)) {
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    link.media = 'all';
+    head.appendChild(link);
+  }
+}
+
+function loadJs(url: string) {
+  $.getScript( url, function( data, textStatus, jqxhr ) {
+    console.log( data ); // Data returned
+    console.log( textStatus ); // Success
+    console.log( jqxhr.status ); // 200
+    console.log( "Load was performed." );
+  });
+}
