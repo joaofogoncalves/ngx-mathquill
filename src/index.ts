8bf8a1324f3bfe65952a94450d1401bc24e274aa
bersling <bersling@gmail.com>
2017-06-20 15:48:29 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import {HelloWorld} from 'hwrld'
+import * as $ from 'jquery'
 
 HelloWorld.sayHello();
+
+export namespace logJquery {
+  export function logJquery () {
+    console.log($, $.ajax);
+  }
+}
