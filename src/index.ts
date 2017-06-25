750ef099a6691bd85687ec604498fcc256d6f8e3
bersling <bersling@gmail.com>
2017-06-25 23:45:16 +0200

    add more methods

--- src/index.ts
+++ src/index.ts
@@ -40,8 +40,8 @@ function loadJs(url: string, callback: (mathquill: IMathQuill) => any): void {
 }
 
 export interface IMathQuill {
-  StaticMath: (htmlElement: HTMLElement) => void;
-  MathField: (htmlElement: HTMLElement, config?: IMathQuill) => void;
+  StaticMath: (htmlElement: HTMLElement) => BaseMethods;
+  MathField: (htmlElement: HTMLElement, config?: IMathQuill) => MathFieldMethods;
   getInterface: (version: 1 | 2) => IMathQuill;
   noConflict: () => IMathQuill;
   config: (MathQuillConfig) => void;
@@ -64,3 +64,27 @@ export interface MathQuillConfig {
     moveOutOf: (dir, mathField) => any;
   }
 }
+
+export interface BaseMethods {
+  revert: () => HTMLElement;
+  reflow: () => void;
+  el: () => HTMLElement;
+  latex: (latex?: string) => void | string;
+}
+
+export interface MathFieldMethods extends BaseMethods {
+  focus: () => void;
+  blur: () => void;
+  write: (latex: string) => void;
+  cmd: (latex: string) => void;
+  select: () => void;
+  clearSelection: () => void;
+  moveToLeftEnd: () => void;
+  moveToRightEnd: () => void;
+  movetoDirEnd: (direction: any) => void;
+  keystroke: (keys: string) => void;
+  typedText: (text: string) => void;
+  config: (config: MathQuillConfig) => void;
+  dropEmbedded: (pageX, pageY, options) => void;
+}
+
